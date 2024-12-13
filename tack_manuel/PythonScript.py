f650a7ea34062f76a3dbf7a7283b9226799aa998
Manuel Tack <tamanu0510@gmail.com>
2024-12-13 10:30:02 +0100

    PAP.jpg erstellt

--- tack_manuel/PythonScript.py
+++ tack_manuel/PythonScript.py
@@ -2,12 +2,17 @@
 print("Hello World")
 
 #For-Schleife von 0 bis 4000
-for i in range(1, 4001):
+# for i in range(1, 4001):
+#     print(f"{i} Hello World")
+
+i = 0
+while(i < 100):
     print(f"{i} Hello World")
+    i += 1
 
 class hallo():
 
     def __init__(self):
         print("Hello World")
 
-hallo()
+#hallo()
