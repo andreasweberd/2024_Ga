7ccb91ffc211e5b45b9abf232489f08478f16ebc
Manuel Tack <tamanu0510@gmail.com>
2024-12-13 09:18:37 +0100

    OOP hinzugefügt

--- tack_manuel/PythonScript.py
+++ tack_manuel/PythonScript.py
@@ -4,3 +4,10 @@ print("Hello World")
 #For-Schleife von 0 bis 4000
 for i in range(1, 4001):
     print(f"{i} Hello World")
+
+class hallo():
+
+    def __init__(self):
+        print("Hello World")
+
+hallo()
