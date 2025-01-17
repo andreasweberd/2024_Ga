f77537752d9d07b1085043313f9d2f83299e4c78
EricEURESA <eric@euresa.de>
2025-01-17 08:50:48 +0100

    Beispielaufgabe Seite 535

---
+++ lawen_eric/Seite535Beispielaufgabe.py
@@ -0,0 +1,14 @@
+inputcount = int(input("Anzahl der Eingaben: "))
+
+array = []
+
+def eingabe():
+    for x in range(inputcount):
+        array.append(float(input("Temperaturwert "+str(x)+": ")))
+
+def average(x):
+    return sum(x)/len(x)
+
+eingabe()
+
+print("Durschnittstemperatur: "+str(average(array)))
