94fb2c193c15ad9a58f27525849caf3112e7910d
Ivan Shuliak <retuhcaf@gmail.com>
2025-01-17 09:01:36 +0100

    Function Temperatur

--- shuliak_ivan/DurchschnittTemperatur.py
+++ shuliak_ivan/DurchschnittTemperatur.py
@@ -1,9 +1,13 @@
-temperaturen = []
+def calculate_average_temperature():
+    temperaturen = []
 
-for i in range(3):
-    temp = float(input(f"Eingabe temperatur{i+1}: "))
-    temperaturen.append(temp)
+    for i in range(3):
+        temp = float(input(f"Eingabe temperatur{i + 1}: "))
+        temperaturen.append(temp)
 
-durchschnitt = sum(temperaturen) / len(temperaturen)
+    durchschnitt = sum(temperaturen) / len(temperaturen)
+    print(f"Durchschnitt: {durchschnitt}")
 
-print(f"Durchschnitt: {durchschnitt}")
+    return durchschnitt
+
+average = calculate_average_temperature()
