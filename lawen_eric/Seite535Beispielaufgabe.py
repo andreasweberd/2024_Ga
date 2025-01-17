6e41f2a911c4c38789e94e32ea89c6df32f6baec
EricEURESA <eric@euresa.de>
2025-01-17 10:20:49 +0100

    Hinzufügen Try Catch Block Beispielaufgabe Seite 535

--- lawen_eric/Seite535Beispielaufgabe.py
+++ lawen_eric/Seite535Beispielaufgabe.py
@@ -1,14 +1,21 @@
-inputcount = int(input("Anzahl der Eingaben: "))
+try:    
+    inputcount = int(input("Anzahl der Eingaben: "))
 
-inputarray = []
+    inputarray = []
 
-def eingabe():
-    for x in range(inputcount):
-        inputarray.append(float(input("Temperaturwert "+str(x+1)+": ")))
+    def eingabe():
+        for x in range(inputcount):
+            inputarray.append(float(input("Temperaturwert "+str(x+1)+": ")))
 
-def average(array):
-    return sum(array)/len(array)
+    def average(array):
+        return sum(array)/len(array)
 
-eingabe()
+    eingabe()
 
-print("Durschnittstemperatur: "+str(average(inputarray)))
+    print("Durschnittstemperatur: "+str(average(inputarray)))
+except:
+    print("Fehlerhafte Eingabe!")
+finally:
+    print("Alle vorherigen Werte:")
+    for singleinput in inputarray:
+        print(singleinput)
