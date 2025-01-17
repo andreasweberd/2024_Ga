3cf084c7a42885210cdcfa7348c489a5d8c5a9a5
Ivan Shuliak <retuhcaf@gmail.com>
2025-01-17 08:47:56 +0100

    Temperatur

---
+++ shuliak_ivan/DurchschnittTemperatur.py
@@ -0,0 +1,7 @@
+temperatur1 = float(input("Eingabe temperatur1: "))
+temperatur2 = float(input("Eingabe temperatur2: "))
+temperatur3 = float(input("Eingabe temperatur3: "))
+
+durchschnitt = (temperatur1 + temperatur2 + temperatur3) / 3
+
+print(f"Durchschnitt: {durchschnitt}")
