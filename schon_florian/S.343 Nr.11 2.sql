18a43be7e002d24572626769345e4adb87b2401b
Florian1324 <118386442+Florian1324@users.noreply.github.com>
2025-03-07 11:16:02 +0100

    Upload S.343 Nr.11 2

---
+++ schon_florian/S.343 Nr.11 2.sql
@@ -0,0 +1,9 @@
+Insert Into Monitor (Name, Preis, Größe) Values('UW3420K', '343,50', '34.0')
+
+Update Monitor Set Preis = Preis * 0.95 Where Größe = '27.0'
+
+Update Monitor Set Name = 'HD2771' Where MonitorID = 4
+
+Update Monitor set Preis = Null Where MonitorId = 12
+
+Delete from Monitor Where Größe = '17.0'
